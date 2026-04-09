37d203332e7350dcec727a189018e5341b958841
lanklaai <lanklaas123+githubai@gmail.com>
2026-04-09 09:43:56 +0200

    Update JNI bridge for jni 0.22 API changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use anyhow::Context;
-use jni::objects::{GlobalRef, JClass, JObject, JObjectArray, JString, JValue};
-use jni::{InitArgsBuilder, JNIVersion, JavaVM};
+use jni::objects::{Global, JClass, JObject, JObjectArray, JString, JValue};
+use jni::{InitArgsBuilder, JNIVersion, JavaVM, jni_sig, jni_str};
 use serde::Deserialize;
 
 #[derive(Debug, thiserror::Error)]
@@ -27,7 +27,7 @@ pub struct CobrixSchema {
 #[derive(Clone)]
 pub struct CobrixJvm {
     vm: Arc<JavaVM>,
-    bridge: GlobalRef,
+    bridge: Arc<Global<JObject<'static>>>,
 }
 
 pub struct CobrixBatchReader {
@@ -38,57 +38,54 @@ pub struct CobrixBatchReader {
 impl CobrixJvm {
     /// Create a JVM and initialize the Scala bridge object.
     pub fn new(classpath: &str) -> anyhow::Result<Self> {
+        let classpath_opt = format!("-Djava.class.path={classpath}");
         let jvm_args = InitArgsBuilder::new()
-            .version(JNIVersion::V8)
-            .option(&format!("-Djava.class.path={classpath}"))
+            .version(JNIVersion::V1_8)
+            .option(&classpath_opt)
             .build()
             .context("failed to build JVM args")?;
 
         let vm = Arc::new(JavaVM::new(jvm_args).context("failed to create JVM")?);
-        let mut env = vm
-            .attach_current_thread()
+        let bridge = vm
+            .attach_current_thread(|env| {
+                let cls = env
+                    .find_class(jni_str!("com/cobrixjni/CobrixBridge"))
+                    .context("cannot find com.cobrixjni.CobrixBridge")?;
+                let obj = env
+                    .new_object(cls, jni_sig!("()V"), &[])
+                    .context("cannot create CobrixBridge")?;
+                env.new_global_ref(obj)
+                    .context("cannot globalize CobrixBridge")
+            })
             .context("failed to attach JNI thread")?;
 
-        let cls = env
-            .find_class("com/cobrixjni/CobrixBridge")
-            .context("cannot find com.cobrixjni.CobrixBridge")?;
-        let obj = env
-            .new_object(cls, "()V", &[])
-            .context("cannot create CobrixBridge")?;
-        let bridge = env
-            .new_global_ref(obj)
-            .context("cannot globalize CobrixBridge")?;
-
-        Ok(Self { vm, bridge })
+        Ok(Self {
+            vm,
+            bridge: Arc::new(bridge),
+        })
     }
 
     pub fn schema_from_copybook(&self, copybook_path: &str) -> Result<CobrixSchema> {
-        let mut env = self
-            .vm
-            .attach_current_thread()
-            .map_err(CobrixJniError::from)?;
-        let path = env
-            .new_string(copybook_path)
-            .map_err(CobrixJniError::from)?;
-        let path_obj = JObject::from(path);
+        self.vm.attach_current_thread(|env| {
+            let path = env.new_string(copybook_path).map_err(CobrixJniError::from)?;
+            let path_obj = JObject::from(path);
 
-        let out = env
-            .call_method(
-                self.bridge.as_obj(),
-                "schemaJson",
-                "(Ljava/lang/String;)Ljava/lang/String;",
-                &[JValue::Object(&path_obj)],
-            )
-            .and_then(|v| v.l())
-            .map_err(CobrixJniError::from)?;
+            let out = env
+                .call_method(
+                    self.bridge.as_obj(),
+                    jni_str!("schemaJson"),
+                    jni_sig!("(Ljava/lang/String;)Ljava/lang/String;"),
+                    &[JValue::Object(&path_obj)],
+                )
+                .and_then(|v| v.l())
+                .map_err(CobrixJniError::from)?;
 
-        let json = env
-            .get_string(&JString::from(out))
-            .map_err(CobrixJniError::from)?
-            .to_string_lossy()
-            .into_owned();
+            let out = env.cast_local::<JString>(out).map_err(CobrixJniError::from)?;
+            let json_chars = out.mutf8_chars(env).map_err(CobrixJniError::from)?;
+            let json = json_chars.to_str().into_owned();
 
-        serde_json::from_str(&json).map_err(CobrixJniError::from)
+            serde_json::from_str(&json).map_err(CobrixJniError::from)
+        })
     }
 
     pub fn open_batch_reader(
@@ -97,22 +94,16 @@ impl CobrixJvm {
         data_path: &str,
         batch_size: i32,
     ) -> Result<CobrixBatchReader> {
-        let mut env = self
-            .vm
-            .attach_current_thread()
-            .map_err(CobrixJniError::from)?;
-        let copybook = env
-            .new_string(copybook_path)
-            .map_err(CobrixJniError::from)?;
-        let data = env.new_string(data_path).map_err(CobrixJniError::from)?;
-        let copybook_obj = JObject::from(copybook);
-        let data_obj = JObject::from(data);
+        let handle = self.vm.attach_current_thread(|env| {
+            let copybook = env.new_string(copybook_path).map_err(CobrixJniError::from)?;
+            let data = env.new_string(data_path).map_err(CobrixJniError::from)?;
+            let copybook_obj = JObject::from(copybook);
+            let data_obj = JObject::from(data);
 
-        let handle = env
-            .call_method(
+            env.call_method(
                 self.bridge.as_obj(),
-                "openReader",
-                "(Ljava/lang/String;Ljava/lang/String;I)J",
+                jni_str!("openReader"),
+                jni_sig!("(Ljava/lang/String;Ljava/lang/String;I)J"),
                 &[
                     JValue::Object(&copybook_obj),
                     JValue::Object(&data_obj),
@@ -120,7 +111,8 @@ impl CobrixJvm {
                 ],
             )
             .and_then(|v| v.j())
-            .map_err(CobrixJniError::from)?;
+            .map_err(CobrixJniError::from)
+        })?;
 
         Ok(CobrixBatchReader {
             jvm: self.clone(),
@@ -132,58 +124,52 @@ impl CobrixJvm {
 impl CobrixBatchReader {
     /// Returns None when the file is fully consumed.
     pub fn next_batch(&mut self) -> Result<Option<Vec<String>>> {
-        let mut env = self
-            .jvm
-            .vm
-            .attach_current_thread()
-            .map_err(CobrixJniError::from)?;
-
-        let arr_obj = env
-            .call_method(
-                self.jvm.bridge.as_obj(),
-                "nextBatchJson",
-                "(J)[Ljava/lang/String;",
-                &[JValue::Long(self.handle)],
-            )
-            .and_then(|v| v.l())
-            .map_err(CobrixJniError::from)?;
-
-        if arr_obj.is_null() {
-            return Ok(None);
-        }
-
-        let arr = JObjectArray::from(arr_obj);
-        let len = env.get_array_length(&arr).map_err(CobrixJniError::from)?;
-        let mut rows = Vec::with_capacity(len as usize);
-        for i in 0..len {
-            let s = env
-                .get_object_array_element(&arr, i)
+        self.jvm.vm.attach_current_thread(|env| {
+            let arr_obj = env
+                .call_method(
+                    self.jvm.bridge.as_obj(),
+                    jni_str!("nextBatchJson"),
+                    jni_sig!("(J)[Ljava/lang/String;"),
+                    &[JValue::Long(self.handle)],
+                )
+                .and_then(|v| v.l())
                 .map_err(CobrixJniError::from)?;
-            let js = JString::from(s);
-            rows.push(
-                env.get_string(&js)
-                    .map_err(CobrixJniError::from)?
-                    .to_string_lossy()
-                    .into_owned(),
-            );
-        }
-        Ok(Some(rows))
+
+            if arr_obj.is_null() {
+                return Ok(None);
+            }
+
+            let arr = env
+                .cast_local::<JObjectArray<'_, JString>>(arr_obj)
+                .map_err(CobrixJniError::from)?;
+            let len = arr.len(env).map_err(CobrixJniError::from)?;
+            let mut rows = Vec::with_capacity(len);
+            for i in 0..len {
+                let js = arr
+                    .get_element(env, i)
+                    .map_err(CobrixJniError::from)?;
+                rows.push(
+                    js.mutf8_chars(env)
+                        .map_err(CobrixJniError::from)?
+                        .to_str()
+                        .into_owned(),
+                );
+            }
+            Ok(Some(rows))
+        })
     }
 
     pub fn close(&mut self) -> Result<()> {
-        let mut env = self
-            .jvm
-            .vm
-            .attach_current_thread()
+        self.jvm.vm.attach_current_thread(|env| {
+            env.call_method(
+                self.jvm.bridge.as_obj(),
+                jni_str!("closeReader"),
+                jni_sig!("(J)V"),
+                &[JValue::Long(self.handle)],
+            )
             .map_err(CobrixJniError::from)?;
-        env.call_method(
-            self.jvm.bridge.as_obj(),
-            "closeReader",
-            "(J)V",
-            &[JValue::Long(self.handle)],
-        )
-        .map_err(CobrixJniError::from)?;
-        Ok(())
+            Ok(())
+        })
     }
 }
 
